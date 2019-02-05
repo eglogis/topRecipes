ee2b9c114632f1898a06b87098fb18fd97d63cc4
eglogis <samu6333@gmail.com>
2019-02-05 12:51:45 +0100

    sube foto
    recibe foto y la sube a la base de datos

--- anadir.php
+++ anadir.php
@@ -3,7 +3,7 @@
 	include_once 'apiUsuario.php';
     $api = new ApiUsuarios();
 
-        $item = array(
+        /*$item = array(
             
                 "login" => $_POST['login'],
                 'contrasena' => $_POST['contrasena'],
@@ -18,5 +18,27 @@
                 'comentarios' => $_POST['comentarios']
             );
 
+         $api->add($item);*/
+
+         if($api->subirImagen($_FILES['foto'])){
+
+         $item = array(
+            
+                "login" => $_POST['login'],
+                'contrasena' => $_POST['contrasena'],
+                'nombre' => $_POST['nombre'],
+                'apellido' => $_POST['apellido'],
+                'nacimiento' => $_POST['nacimiento'],
+                'correo' => $_POST['correo'],
+                'latitud' => $_POST['latitud'],
+                'altitud' => $_POST['altitud'],
+                'telefono' => $_POST['telefono'],
+                'foto' => $api->getImagen(),
+                'comentarios' => $_POST['comentarios']
+            );
+
          $api->add($item);
+     }else{
+            $api->error('Error con el archivo: ' . $api->getError());
+        }
 ?>
--- apiUsuario.php
+++ apiUsuario.php
@@ -4,6 +4,9 @@ include_once 'usuario.php';
 
 class ApiUsuarios{
 
+    private $imagen;
+    private $error;
+
     function getAll(){
         $usuario = new Usuario();
         $usuarios = array();
@@ -100,6 +103,55 @@ class ApiUsuarios{
     function exito($mensaje){
 
         echo json_encode(array('mensaje' => $mensaje));
+    }
+
+    function subirImagen($file){
+
+        $directorio = "imagenes/";
+
+        $this->imagen = basename($file["name"]);
+        $archivo = $directorio . basename($file["name"]);
+
+        $tipoArchivo = strtolower(pathinfo($archivo, PATHINFO_EXTENSION));
+
+        $checarSiImagen = getimagesize($file["tmp_name"]);
+
+        if($checarSiImagen != false){
+            //validando tamaño del archivo
+            $size = $file["size"];
+
+            if($size > 500000){
+                $this->error = "El archivo tiene que ser menor a 500kb";
+                return false;
+            }else{
+                //validar tipo de imagen
+                if($tipoArchivo == "jpg" || $tipoArchivo == "jpeg" || $tipoArchivo == "png"){
+                    // se validó el archivo correctamente
+                    if(move_uploaded_file($file["tmp_name"], $archivo)){
+                        //echo "El archivo se subió correctamente";
+                        return true;
+                    }else{
+                        $this->error = "Hubo un error en la subida del archivo";
+                        return false;
+                    }
+                }else{
+                    $this->error = "Solo se admiten archivos jpg/jpeg";
+                    return false;
+                }
+            }
+        }else{
+            $this->error = "El documento no es una imagen";
+            return false;
+        }
+    }
+
+    function getImagen(){
+
+        return $this->imagen;
+    }
+
+    function getError(){
+        return $this->error;
     }
 }
 ?>
